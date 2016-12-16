672cfa00a1afcbf75949f41b1522b0726a9d3f4d
Blake Tan <koonchong@gmail.com>
2016-12-15 23:43:06 -0800

    update readme, and implement server side validations on data

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -3,6 +3,11 @@ class Responder < ApplicationRecord
   geocoded_by :full_street_address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
 
+  validates :fname,:lname, presence: true
+  validates :phone, presence: true, format: { with: /\+1[0-9]{10}/ , message: "Please provide number in format +1XXXYYYZZZZ"}, uniqueness: true
+  validates :email, presence: true, format: { with: /@/ }
+  validates :address, :city, :state, :zip, presence: true
+
   def full_street_address
     return "#{address}, #{city}, #{state}"
   end
